1fb94d8068f1d13720240b8c6b7eefec9910403d
daniel <danmoreira.dev@gmail.com>
2026-07-23 15:25:56 -0300

    refactor: replace require with assert in test cases and improve code structure

--- dsa/sort_algoritm_test.go
+++ dsa/sort_algoritm_test.go
@@ -1,21 +1,33 @@
+// AAA (Arrange, Act, Assert)
 package dsa_test
 
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 
 	"app/dsa"
 )
 
 func Test_Fibonacci(t *testing.T) {
+	// arrange
 	uc := new(dsa.Fibo)
 	uc.Count = 1
+	
+	// act
 	result := uc.Fibonacci(1, 0)
-	require.Equal(t, 4181, result)
+	
+	// assert
+	assert.Equal(t, 4181, result)
 }
 
 func Test_BubbleSort(t *testing.T) {
-	result := dsa.BubbleSort([]int{8, 7, 3, 10, 28, 4})
-	require.Equal(t, []int{3, 4, 7, 8, 10, 28}, result)
+	// arrange
+	list:= []int{8, 7, 3, 10, 28, 4}
+	
+	// act
+	result := dsa.BubbleSort(list)
+
+	// assert
+	assert.Equal(t, []int{3, 4, 7, 8, 10, 28}, result)
 }
